9f925804a8bf3e38ace526133cfbed29ce086c0c
kaushikar11 <kaushikar2211@gmail.com>
2024-09-29 19:44:26 +0530

    removed stuff

--- src/firebase.js
+++ src/firebase.js
@@ -10,13 +10,13 @@ import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCjyxWjqmsWAOfXeKF8sG751vXBkQnc33I",
-  authDomain: "ladieshosteltce.firebaseapp.com",
-  projectId: "ladieshosteltce",
-  storageBucket: "ladieshosteltce.appspot.com",
-  messagingSenderId: "1073771171115",
-  appId: "1:1073771171115:web:e29a9b2dc4efb37fa9096e",
-  measurementId: "G-MG6PN8JE3Y"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
